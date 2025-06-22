575d3e6a4f9d656896a9c62f6c9d1999bd78fb1b
Mohammed-Amran <153233001+Mohammed-Amran@users.noreply.github.com>
2025-06-23 00:39:52 +0300

    Update CartItem.java

--- src/main/java/com/example/model/CartItem.java
+++ src/main/java/com/example/model/CartItem.java
@@ -3,79 +3,108 @@ package com.example.model;
 public class CartItem {
 
 	//Instance variables:
-	private int productId;
+	private int cartItemId;
 	private int userId;
-	private String productName;
-	private double productPrice;
 	private int selectedQuantity;
 	private int productSumPrice; // = (producctPrice * selectedQuantity)
+	private String productName;
+	private int productPrice;
+	private int productId;
 	
 	
-	
+
+
 	//Constructor
-	public CartItem(int userId, int productId, String productName, double productPrice, int selectedQuantity, int productSumPrice) {
-		
-		this.productId = productId;
-		this.userId = userId;
-		this.productName = productName;
-		this.productPrice = productPrice;
-		this.selectedQuantity = selectedQuantity;
-		this.productSumPrice = productSumPrice;
-		
-	}//closing brace of the constructor
+	public CartItem() { }
+
+
 	
-	
-	public void setProductPrice(double productPrice) {
-		
-		this.productPrice = productPrice;
-	}
-	
-	public double getProductPrice() {
-		
-		return productPrice;
-	}
-	
-	
+
 	public int getProductId() {
 		return productId;
 	}
+
+
+
+
 	public void setProductId(int productId) {
 		this.productId = productId;
 	}
-	
-	
+
+
+
+
+	public int getCartItemId() {
+		return cartItemId;
+	}
+
+
+
+	public void setCartItemId(int cartItemId) {
+		this.cartItemId = cartItemId;
+	}
+
+
+
 	public int getUserId() {
 		return userId;
 	}
+
+
+
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-	
-	
 
-	public String getProductName() {
-		return productName;
-	}
-	public void setProductName(String productName) {
-		this.productName = productName;
-	}
-	
-	
+
+
 	public int getSelectedQuantity() {
 		return selectedQuantity;
 	}
+
+
+
 	public void setSelectedQuantity(int selectedQuantity) {
 		this.selectedQuantity = selectedQuantity;
 	}
-	
-	
+
+
+
 	public int getProductSumPrice() {
 		return productSumPrice;
 	}
+
+
+
 	public void setProductSumPrice(int productSumPrice) {
 		this.productSumPrice = productSumPrice;
 	}
-
 	
+	
+	
+	public String getProductName() {
+		return productName;
+	}
+
+
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+
+
+	public int getProductPrice() {
+		return productPrice;
+	}
+
+
+
+	public void setProductPrice(int productPrice) {
+		this.productPrice = productPrice;
+	}
+	
+	
+
 	
 }//closing brace of the class.
