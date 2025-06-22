0ceacb735a7d44a5dcc9e238b96ebe013401e080
Mohammed-Amran <153233001+Mohammed-Amran@users.noreply.github.com>
2025-06-23 00:07:57 +0300

    Update ProductController.java

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -1,11 +1,10 @@
 package com.example.controller;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class ProductController {
@@ -31,24 +30,12 @@ public class ProductController {
 		
 		 //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 		 
-		 //Instantiating a session object
-		 HttpSession session = req.getSession(false);
-		 
-		 //Retrieving the 'cartCounter' variable from the session scope:
-		 int cartCounter = (Integer) session.getAttribute("cartCounter");
-		 
-		 //forwarding & wrapping the 'cartCounter' into the request scope:
-		 model.addAttribute("cartCounter", cartCounter);
-		 
-		 
-		//Retrieving the 'inboxCounter' variable from the session scope:
-		 int inboxCounter = (Integer) session.getAttribute("inboxCounter");
-		 
-		//forwarding & wrapping the 'cartCounter' into the request scope:
-		 model.addAttribute("inboxCounter", inboxCounter);
-		 
+		 		 
 		return "view/home";
 		
 	}//closing brace of the 'homeCategoryController()' method.
+	
+	
+	
 	
 }//closing brace of the class.
