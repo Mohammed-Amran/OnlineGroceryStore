996b51ee99f28449547b5dbbcdf6c2867c0b747a
Mohammed-Amran <153233001+Mohammed-Amran@users.noreply.github.com>
2025-06-23 00:09:19 +0300

    Update checkoutController.java

--- src/main/java/com/example/controller/checkoutController.java
+++ src/main/java/com/example/controller/checkoutController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.model.CartItem;
+import com.example.dao.DaoCartItem;
+import com.example.dao.DaoOrders;
+
+import com.example.model.Orders;
 
 @Controller
 public class checkoutController {
@@ -21,118 +24,147 @@ public class checkoutController {
 	@PostMapping("/checkoutOrder")
 	protected String checkoutOrders(@RequestParam Map<String, String> input, HttpServletRequest req, Model model) {
 		
-		
-		//getting the 'selectedCity' & the 'Address' inputs from the user:
-		String selectedCity = input.get("selectedCity");
-		String Address = input.get("Address");
+		String destination = "";
 		
 		
 		//Instantiating a session object:
 		HttpSession session = req.getSession(false);
 		
-		
-		//saving the 'selectedCity' & the 'Address' into the session scope:
-		session.setAttribute("selectedCity", selectedCity);
-		session.setAttribute("Address", Address);
-		
-		
-		//Retrieving the cartItems list from the session object:
-		@SuppressWarnings("unchecked")
-		List<CartItem> retrievedCartItems = (List<CartItem>) session.getAttribute("cartItemsList");
-		
-		
-		//Retrieving the orderedItems list from the session object:		
-		@SuppressWarnings("unchecked")
-		List<CartItem> orderedItems = (List<CartItem>) session.getAttribute("orderedItems");
-		
-		
-		//copying the cartItems into the orderedItems list:
-		for(CartItem product : retrievedCartItems) {
+		if(session != null) {
+			
+			//Retrieving the 'userId' from the session scope:
+			int userId = (Integer) session.getAttribute("userId");
+			
+			//Getting the inputs from the user:
+			String selectedCity = input.get("selectedCity");
+			
+			String Address = input.get("Address");
+			
+			String productName = input.get("productName");
+			
+			int productPrice = Integer.parseInt( input.get("productPrice") );
+			
+			int selectedQuantity = Integer.parseInt( input.get("selectedQuantity") );
+			
+			int productSumPrice = Integer.parseInt( input.get( "productSumPrice" ) );
+			
+			//Instantiating an object from the 'DaoOrders' class:
+			DaoOrders daoOrdersObj = new DaoOrders();
+			
+			//Calling the 'insertIntoOrders()' method via the daoOrdersObj:
+			boolean isOrderInserted = daoOrdersObj.insertIntoOrders(userId, productName, productPrice, selectedQuantity, productSumPrice, selectedCity, Address);
+			
+			if(isOrderInserted) {
+								
+				//Retrieving the new 'inboxCounter' variable via the daoOrdersObj:
+				int inboxCounter = daoOrdersObj.getOrderedItemsCount(userId);
+				
+				//Re-saving the 'inboxCounter' into the session Scope:
+				session.setAttribute("inboxCounter", inboxCounter);
+				
+				
+				//Clearing the cart after checking out the items:
+				
+				//I.Instantiating an object from the 'DaoCartItem' class:
+				DaoCartItem daoCartItemObj = new DaoCartItem();
+				
+				//II. Calling the 'clearCart()' method via the daoCartItemObj:
+				daoCartItemObj.clearCart(userId);
+				
+				//III. Re-retrieving the 'cartCounter' variable via the daoCartItemObj:
+				int cartCounter = daoCartItemObj.getCartItemCount(userId);
+				
+				//IV. Re-saving the 'cartCounter' variable into the session scope:
+				session.setAttribute("cartCounter", cartCounter);
+				
+				
+				//This code below allows to represent the vegetable products as soon as the gets back to the home page again after checking out:
+				String showCategory = "vegetable";
+		     	model.addAttribute("showCategory", showCategory);
+				
+				
+				destination = "view/home";
+			
+			}
+			else {
+				
+				String orderInsertionError = "Failed to Checkout Items!";
+				
+				model.addAttribute("orderInsertionError", orderInsertionError);
+				
+				destination = "view/error";
+			}
 			
 			
-			orderedItems.add(product);
+			
+			
+		}
+		else {
+			
+			String SessionError = "Sorry! your session has expired";
+			
+			model.addAttribute("SessionError", SessionError);
+			
+			destination = "view/error";
 			
 		}
 		
 		
-		//saving the 'orderedItems' list into the session scope:
-		session.setAttribute("orderedItems", orderedItems);
-		
-		
-		//Clearing out the 'cartItemsList' - since after checking out the cart must be cleared out:
-		retrievedCartItems.clear();
-		
-		//Re-saving the 'cartItemsList' into the session scope again:
-		session.setAttribute("cartItemsList", retrievedCartItems);
-		
-		// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-		
-		//Retrieving the 'inboxCounter' variable from the session scope:
-		int inboxCounter = (Integer) session.getAttribute("inboxCounter");
-		
-		//Incrementing the 'inboxCounter' by 1 - since an order has been checkout!
-		++inboxCounter;
-		
-		//Re-saving the 'inboxCounter' variable into the session scope again:
-		session.setAttribute("inboxCounter", inboxCounter);
-		
-		//forwarding & wrapping the 'inboxCounter' variable into the request scope:
-		model.addAttribute("inboxCounter", inboxCounter);
-		
-		
-		
-		
-		//Retrieving the 'cartCounter' variable from the session scope:
-		int cartCounter = (Integer) session.getAttribute("cartCounter");
-		
-		//re-initializing it to 0 - since after checking out, the cart should be cleared out:
-		cartCounter = 0;
-		
-		//Re-saving the 'cartCounter' variable into the session scope again:
-		session.setAttribute("cartCounter", cartCounter);
-		
-		//forwarding & wrapping the 'cartCounter' variable into the request scope:
-		model.addAttribute("cartCounter", cartCounter);
-		
-		//This code below allows to represent the vegetable products as soon as the gets back to the home page again after checking out:
-		String showCategory = "vegetable";
-     	model.addAttribute("showCategory", showCategory);
-		
-		
-		return "view/home";
+     	
+		return destination;
 		
 	}//closing brace of the 'checkoutOrders()' method.
+	
+	
 	
 	
 	
 	@GetMapping("/accessCheckoutPage")
 	protected String accessChecckoutPage(HttpServletRequest req, Model model) {
 		
+		String destination = "";
+		
 		//instantiating a session object:
 		HttpSession session = req.getSession(false);
 		
-		//Retrieving the orderedItems list from the session scope:
-		@SuppressWarnings("unchecked")
-		List<CartItem> orderedItems = (List<CartItem>) session.getAttribute("orderedItems");
+	
+		if(session != null) {
+			
+			//Retrieving the orderedItems from the 'orders' table:
+			
+			//I.Retrieving the 'userId' from the session scope:
+			int userId = (Integer) session.getAttribute("userId");
+			
+			//II. Instantiating an object from the 'DaoOrders' class:
+			DaoOrders daoOrdersObj = new DaoOrders();
+			
+			//III. Accessing the 'getOrderedItemsByUserId()' method via the daoOrdersObj:
+			List<Orders> orderedItems = (List<Orders>)  daoOrdersObj.getOrderedItemsByUserId(userId);
+			
+			//IV. Saving the 'orderedItems' into the session scope:
+			session.setAttribute("orderedItems", orderedItems);
+			
+			
+			destination = "view/checkout";
 		
-		//forwarding & wrapping the 'orderedItems' into the request scope:
-		model.addAttribute("orderedItems", orderedItems);
+		}
+		else {
+			
+            String SessionError = "Sorry! your session has expired";
+			
+			model.addAttribute("SessionError", SessionError);
+			
+			destination = "view/error";
+		}
+	
 		
-		
-		//Retrieving the Address info from the session scope:
-		String selectedCity = (String) session.getAttribute("selectedCity");
-		String Address = (String) session.getAttribute("Address");
-		
-		//forwarding & wrapping the Address info into the request scope:
-		model.addAttribute("selectedCity", selectedCity);
-		model.addAttribute("Address", Address);
-		
-		
-		return "view/checkout";
+		return destination;
 		
 	}//closing brace of the 'accessCheckoutPage()' method.
 	
 	
+	
+		
 	
 	@GetMapping("/getBackToHomeFromCheckout")
 	protected String getBackToHomeFromCheckout(HttpServletRequest req, Model model) {
@@ -144,24 +176,12 @@ public class checkoutController {
 		
         
         if(session != null) {
+
         	
-        	//Retrieving the 'cartCounter' variable from the session scope:
-    		int cartCounter = (Integer) session.getAttribute("cartCounter");
-            
-    		//forwarding & wrapping the 'cartCounter' variable into the request scope:
-    		model.addAttribute("cartCounter", cartCounter);
-    		
-    		
-    		//Retrieving the 'inobxCounter' variable from the session scope:
-            int inboxCounter = (Integer) session.getAttribute("inboxCounter"); 
-            
-            //forwarding & wrapping the 'cartCounter' variable into the request scope:
-            model.addAttribute("inboxCounter", inboxCounter); 
-             
-            
             //This code below allows to represent the vegetable products as soon as the gets back to the home page again after checking out:
             String showCategory = "vegetable";
             model.addAttribute("showCategory", showCategory);
+            
             
             destination = "view/home";
         }
