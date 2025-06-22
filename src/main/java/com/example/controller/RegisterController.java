6ec6dd68467e46fcd065d5904cd4b6d9f23b0df7
Mohammed-Amran <153233001+Mohammed-Amran@users.noreply.github.com>
2025-06-23 00:08:29 +0300

    Update RegisterController.java

--- src/main/java/com/example/controller/RegisterController.java
+++ src/main/java/com/example/controller/RegisterController.java
@@ -1,9 +1,9 @@
 package com.example.controller;
 
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -13,7 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.example.model.CartItem;
+
+import com.example.dao.*;
 
 
 
@@ -39,62 +40,112 @@ public class RegisterController {
 	
 	
 	
-	@PostMapping("/Register")
+	@PostMapping("/registerUser")
 	protected String registerUser(@RequestParam Map<String, String> input, HttpServletRequest req, HttpServletResponse resp, Model model) {
 		
 		String destination = "";
 		
+		//Retrieving the user inputs:
 		String userName = input.get("username");
+		
 		String email = input.get("email");
+		
 		String password = input.get("password");
 		
 		
+		//Authenticate the email:
 		
 	
+		boolean isEmailValid = false;
 		
-		if( ("testuser1".equals(userName)) && ("testuser@gmail.com".equals(email)) && ("test12345".equals(password)) ) {
+		
+		//Email Validation Reference:
+		final Pattern EMAIL_REGEX =  Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+		
+		
+		//Checking if the inputed 'email' is Authenticated? or not!
+		if(EMAIL_REGEX.matcher(email).matches()) {
+			
+			isEmailValid = true;
+		}
+		else {
+			
+			isEmailValid = false;
+		}
+	
+		
+		if(isEmailValid == false) {
+			
+			String emailError = "Make sure your email is Correct";
+			
+			model.addAttribute("emailError", emailError);
+			
+			
+			//Saving the username, email and password into the request Scope!
+			//So that the user wont required to fill-in all the inputs again:
+			model.addAttribute("username", userName);
+			
+			model.addAttribute("email", email);
+			
+			model.addAttribute("password", password);
+			
+			
+			destination = "view/home";
+		}
+		else{
 			 
-			 
+			    //Instantiating a session object:
 			    HttpSession session = req.getSession(false);
 			    
-			    session.setAttribute("userName", userName);
-			    session.setAttribute("email", email);
-			    session.setAttribute("userId", 1);
-			  
-			    List<CartItem> cartItemsList = new ArrayList<>();
-			    List<CartItem> orderedItems = new ArrayList<>(); 
-		    	
-			    int cartCounter = 0;
-			    int inboxCounter = 0;
-		    	
-		    	session.setAttribute("cartItemsList", cartItemsList);
-		    	session.setAttribute("orderedItems", orderedItems);
-			   
-		    	session.setAttribute("cartCounter", cartCounter);
-		    	session.setAttribute("inboxCounter", inboxCounter);
 			    
-			    model.addAttribute("cartCounter", cartCounter);
+			    //Inserting user into 'users' table:
+			    
+			    //I. Instantiating an object from 'DaoUsers' class:
+			    DaoUser daoUsersObj = new DaoUser();
+			    
+			    //II. Calling the 'insertUser()' method via the daoUsersObj:
+			    boolean isUserInserted = daoUsersObj.insertUser(userName, email, password);
+			    
+			    
+			    if(isUserInserted) {
+			    	
+			    	//Retrieve the userId from the 'users' table:
+			    	int userId = daoUsersObj.retrieveId(email);
+			    	
+			    	//Saving all the necessary info's into the session scope:
+			    	session.setAttribute("userName", userName);
+			    	
+					session.setAttribute("email", email);
+					
+					session.setAttribute("userId", userId);
+					
+					
+					//Retrieving the inboxCounter from 'orders' table:
+					int inboxCounter = 0;
+					
+					
+					//I. Instantiating an object from the 'DaoOrders' class:
+					DaoOrders daoOrdersObj = new DaoOrders();
+					
+					//II. Calling the 'getOrderedItemsCount()' method via the daoOrdersObj:	
+					inboxCounter = daoOrdersObj.getOrderedItemsCount(userId);
+					
+					//III. Saving the 'inboxCounter' variable into the session scope:
+					session.setAttribute("inboxCounter", inboxCounter);
+					
+					
+					 destination = "view/home";
+			    	
+			    }
+		    
 			    
 			    //This code below allows to show the vegetable products as soon as the user access the Grocery Store web app.
 			    String showCategory = "vegetable";
 			  
 			    model.addAttribute("showCategory", showCategory);
-			    
-			    			    
-			
-			    destination = "view/home";
+			    			   
 		}
-		else {
-			 
-			
-			String registrationError = "Make sure that your email correct!";
-			model.addAttribute("registrationError", registrationError);
-			
-				
-			destination = "view/error";
-		        
-		    }
-
+		
 		
       return destination;	
 				
